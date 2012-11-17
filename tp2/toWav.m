2cb46d553cfa80567c4d05828fd3da13e749a1c4
José Galindo <joseignaciosg9@gmail.com>
2012-11-17 20:52:54 -0300

    obtiene el archivo pero no se escucha bien

--- tp2/toWav.m
+++ tp2/toWav.m
@@ -24,7 +24,7 @@ disp(min(frecs));
 
 fs = 1/Tm;
 nsamples = length(triplets);
-f = ((1:nsamples) -ceil (nsamples/2) )/nsamples /(1/fs);
+f = ((1:nsamples) )/nsamples /(1/fs);
 
 plot(f,frecs);
 
@@ -33,18 +33,29 @@ plot(f,frecs);
 %antitransformating frequencies
 notes = ifft(frecs);
 
+n = 431790/nsamples ;
+
+%fix to have se same quantity of nytes as before
+k = 1;
+for i=1 : nsamples
+	for j=k : n*i	
+		salida(k++) = real( notes(i) );
+	endfor
+endfor
+
+N = 431790;
+
+f= (1:N) / N / Tm;
+
+%plot(f,salida)
+
+length(salida)
+
+
 %creating the wavifft(frecs)
-wavwrite(notes, Tm, 'test.wav');
+wavwrite(salida', 11025, 'test.wav');
 
 %plot(notes);
-
-
-
-
-
-
-
-
 
 endfunction
 
